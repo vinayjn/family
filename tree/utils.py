2a408ae92f173e09170a3a501dc1e7bc92587fab
Vinay Jain <vinay.jn7@gmail.com>
2019-04-28 00:34:24 +0530

    Added graph

--- tree/utils.py
+++ tree/utils.py
@@ -3,17 +3,28 @@ import json
 from .models import Member
 
 
-def get_json(root, members):
-	data = {}
-	data["nodeName"] = root.name
+def get_json(root, members, node_id, depth):
+	data = {}	
 	data["children"] = []
-	children = sorted(list(filter(lambda x: x.parent == root, members)), key=lambda x: x.order)
-	for child in children:
-		data["children"].append(get_json(child, members))
+	data["nodeName"] = root.name
+	data["name"] = root.name	
+	data["code"] = "N" + node_id
+	data["type"] = "type" + str(depth)
+	data["label"] = root.name
+	data["version"] = "v1.0"
+	data["link"] = {
+		"name" : "Link NODE NAME 1",
+		"nodeName" : root.name,
+		"direction" : "ASYN"
+	}
+	children = sorted(list(filter(lambda x: x.parent == root, members)), key=lambda x: x.id)
+	for index, child in enumerate(children):
+		node_id = str(depth + 1) + "." + str(index + 1)
+		data["children"].append(get_json(child, members, node_id, depth))
 	return data
 
 def get_json_tree():
 	members = Member.objects.all()
 	root = members[0]
-	json_data = get_json(root, members)
-	return json.dumps(json_data)
+	json_data = get_json(root, members, "1", 1)	
+	return json.dumps({"tree": json_data})
--- tree/views.py
+++ tree/views.py
@@ -4,4 +4,9 @@ from django.http import HttpResponse
 from .utils import get_json_tree
 
 def index(request):
+	context = {"json_tree": get_json_tree()}
+	return render(request, 'tree/index.html', context)	
+
+def fetch_json_tree(request):
 	return HttpResponse(get_json_tree())
+    
