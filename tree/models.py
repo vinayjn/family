7bed6791e0b55714efe1f189fd1bb5aa9dd7f9e9
Vinay Jain <vinay.jn7@gmail.com>
2019-04-27 18:14:47 +0530

    Removing ignored files

--- tree/models.py
+++ tree/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 class Member(models.Model):
 	name = models.CharField(max_length=200)
-	picture_url = models.TextField(null=True)
-	parent = models.ForeignKey('self', null=True, on_delete=models.CASCADE)
+	picture_url = models.TextField(blank=True, null=True)
+	parent = models.ForeignKey('self', null=True, blank=True,  on_delete=models.CASCADE)
 
 	def __str__(self):
 		return self.name
