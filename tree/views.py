891306a0e8d02e8abf868da0a94f2c8447aca654
Vinay Jain <vinay.jn7@gmail.com>
2019-04-27 20:41:42 +0530

    Fixed lint issue

--- tree/views.py
+++ tree/views.py
@@ -4,6 +4,4 @@ from django.http import HttpResponse
 from .utils import get_json_tree
 
 def index(request):
-	members = Member.objects.all()
-	queue = [members[0]]
-    return HttpResponse(get_json_tree())
+	return HttpResponse(get_json_tree())
