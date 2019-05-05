423a474152b266d40843d8302d8697dc586d5c94
Vinay Jain <vinay.jn7@gmail.com>
2019-05-05 15:32:39 +0530

    Added Authentication

--- tree/views.py
+++ tree/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
+
 from .utils import get_json_tree
 
+
+@login_required(login_url='/admin/login/?next=/')
 def index(request):
 	context = {"json_tree": get_json_tree()}
 	return render(request, 'tree/index.html', context)	
 
+@login_required(login_url='/admin/login/?next=/')
 def fetch_json_tree(request):
 	return HttpResponse(get_json_tree())
     
